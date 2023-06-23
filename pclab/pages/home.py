93a2bfd91a2083959784b6da70ab52ccc61b3171
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-23 01:47:18 +0000

    update

--- pclab/pages/home.py
+++ pclab/pages/home.py
@@ -158,10 +158,10 @@ def update_figure(pattern):
     Output("label", "value"),
     Output("label", "disabled"),
     Output("image", "src"),
-    Input("graph", "selectedData"),
+    Input("graph", "clickData"),
     prevent_initial_call=True,
 )
-def update_selected(data):
+def update_clicked(data):
     if not isinstance(data, dict):
         None, True, None
     id = data["points"][0]["customdata"]
