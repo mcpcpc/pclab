07d2bc63f2ac16d9e4261f3b6f266fb3da4652d2
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-23 12:18:29 -0400

    update

--- pclab/__init__.py
+++ pclab/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from dash import Dash
 from dash import page_registry
 
+from pclab.cache import create_cache_manager
 from pclab.db import init_app
 from pclab.layout.default import layout
 
@@ -29,11 +30,13 @@ def create_app(test_config=None):
     except OSError:
         pass
     init_app(app)
+    manager = create_cache_manager(app)
     dashapp = Dash(
         __name__,
         server=app,
         use_pages=True,
         update_title=None,
+        background_callback_manager=manager,
     )
     dashapp.layout = layout()
     return dashapp.server
