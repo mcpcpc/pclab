ec2699fa1e9826dec496d84c51846bfb42f6d5c4
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-25 01:18:45 +0000

    update

--- pclab/pages/home.py
+++ pclab/pages/home.py
@@ -10,21 +10,16 @@ from dash import no_update
 from dash import State
 from dash import register_page
 from dash_iconify import DashIconify
-from dash_mantine_components import Button
 from dash_mantine_components import Col
 from dash_mantine_components import Image
 from dash_mantine_components import Grid
 from dash_mantine_components import Group
 from dash_mantine_components import LoadingOverlay
 from dash_mantine_components import NavLink
-from dash_mantine_components import Notification
 from dash_mantine_components import SegmentedControl
-from dash_mantine_components import TextInput
 
 from pclab.db import get_db
 from pclab.utils.figure import create_pca_figure
-from pclab.utils.common import get_files
-from pclab.utils.common import to_binary
 from pclab.utils.model import create_model
 from pclab.utils.preprocess import to_array
 from pclab.utils.preprocess import to_image
@@ -37,7 +32,6 @@ register_page(
 )
 
 layout = [
-    html.Div(id="notify_load"),
     Grid(
         p="md",
         children=[
@@ -46,25 +40,6 @@ layout = [
                 xs=12,
                 p="md",
                 children=[
-                    TextInput(
-                        id="pattern",
-                        label="Directory Pattern",
-                        description="Accepts absolute or relative path.",
-                        pb="xl",
-                        icon=DashIconify(icon="carbon:image-search"),
-                        placeholder="i.e. ./instance/*/*.PNG",
-                        value="./instance/*/*.PNG",
-                    ),
-                    NavLink(
-                        id="load",
-                        icon=DashIconify(icon="ic:outline-upload-file"),
-                        label="Load Files",
-                    ),
-                    NavLink(
-                        id="clear",
-                        icon=DashIconify(icon="ic:baseline-clear"),
-                        label="Clear Database",
-                    ),
                     NavLink(
                         id="refresh",
                         icon=DashIconify(icon="ic:baseline-refresh"),
@@ -126,56 +101,6 @@ def update_label_data(value):
     rows = get_db().execute("SELECT title, id FROM label").fetchall()
     data = [{"label": r["title"], "value": str(r["id"])} for r in rows]
     return data
-
-
-@callback(
-    output=Output("notify_load", "children"),
-    inputs=[
-        Input("load", "n_clicks"),
-        State("pattern", "value"),
-    ],
-    background=True,
-    running=[
-        (Output("pattern", "disabled"), True, False),
-        (Output("load", "disabled"), True, False),
-        (Output("clear", "disabled"), True, False),
-        (Output("refresh", "disabled"), True, False),
-    ],
-    prevent_initial_call=True,
-)
-def load_files(n_clicks, pattern):
-    paths = get_files(pattern)
-    db = get_db()
-    for path in paths:
-        blob = to_binary(path)
-        try:
-            db.execute("PRAGMA foreign_keys = ON")
-            db.execute(
-                """
-                INSERT INTO sample (
-                    filename, blob
-                ) VALUES (?, ?)
-                """,
-                (path, blob)
-            )
-            db.commit()
-        except db.Error as error:
-            return Notification(
-                id="warning",
-                icon=DashIconify(icon="ic:baseline-warning"),
-                title="Warning",
-                message=f"{error}",
-                color="yellow",
-                action="show",
-            )
-    return Notification(
-        id="complete",
-        icon=DashIconify(icon="ic:round-celebration"),
-        title="Complete",
-        message="All images processed.",
-        color="blue",
-        action="show",
-    )
     
 
 @callback(
@@ -234,28 +159,18 @@ def update_figure(n_clicks):
     rows = get_db().execute(
         """
         SELECT
-            id,
-            label_id,
-            blob
+            sample.id AS id,
+            sample.label_id AS label_id,
+            sample.blob AS blob,
+            label.title as label_title
         FROM sample
+            INNER JOIN label ON label.id = sample.label_id
         """
     ).fetchall()
     if len(rows) < 1:
         return no_update
     model = create_model()
-    ids, labels, blobs = zip(*map(lambda x: dict(x).values(), rows))
+    ids, labels, blobs, titles = zip(*map(lambda x: dict(x).values(), rows))
     pcs = model.fit_transform(list(map(to_array, blobs)))
-    figure = create_pca_figure(ids, labels, pcs)
+    figure = create_pca_figure(ids, labels, pcs, titles)
     return figure
-
-
-@callback(
-    Output("clear", "disabled"),
-    Input("clear", "n_clicks"),
-    prevent_initial_call=True,
-)
-def update_cleared(n_clicks):
-    db = get_db()
-    db.execute("DELETE FROM sample")
-    db.commit()
-    return no_update
