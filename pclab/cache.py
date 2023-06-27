a76a4684d5e2edefaf1f2778eaf1b8d38f7a0ce2
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-27 17:40:15 -0400

    update

--- pclab/cache.py
+++ pclab/cache.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from os import path
+from uuid import uuid4
 
 from dash import CeleryManager
 from dash import DiskcacheManager
 from diskcache import Cache
 from celery import Celery
+
+launch_uid = uuid4()
 
 
 def create_cache_manager(app):
@@ -15,9 +18,15 @@ def create_cache_manager(app):
             __name__,
             broker=app.config.get("REDIS_URL"),
             backend=app.config.get("REDIS_URL"),
+            cache_by=[lambda: launch_uid],
+            expire=60,
         )
         manager = CeleryManager(celery_app)
     else:
-        cache = Cache(path.join(app.instance_path, ".cache"))
+        cache = Cache(
+            path.join(app.instance_path, ".cache")
+            cache_by=[lambda: launch_uid],
+            expire=60,
+        )
         manager = DiskcacheManager(cache)
     return manager
