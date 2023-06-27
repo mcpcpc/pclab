5e4d495e129f62abab965616fd438bbe3c94a238
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-27 14:28:59 -0400

    Update __init__.py

--- pclab/__init__.py
+++ pclab/__init__.py
@@ -14,7 +14,7 @@ from pclab.cache import create_cache_manager
 from pclab.db import init_app
 from pclab.layout.default import layout
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 def create_app(test_config=None):
