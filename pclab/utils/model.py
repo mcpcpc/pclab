52d838297c8e3ae25892800f9f530ca2cb5c1939
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-06-29 21:31:56 -0400

    update

--- pclab/utils/model.py
+++ pclab/utils/model.py
@@ -4,6 +4,7 @@
 #from sklearn.decomposition import PCA
 from sklearn.decomposition import IncrementalPCA
 
-def create_model() -> PCA:
+#def create_model() -> PCA:
     #return PCA(n_components=2, random_state=0)
+def create_model() -> IncrementalPCA: 
     return IncrementalPCA(n_components=2)
