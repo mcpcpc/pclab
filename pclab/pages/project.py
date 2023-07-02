37fd7e40abe1b9c9437f9fb0a3bab3d6ee76c1d3
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-07-02 17:52:15 -0400

    update

--- pclab/pages/project.py
+++ pclab/pages/project.py
@@ -135,7 +135,7 @@ def update_row_request(request, selected_data):
         return no_update
     if len(selected_data["points"]) < 1:
         #return no_update
-        return {"rowData": [], "rowCount": 0}
+        return {"rowData": [], "rowCount": None}
     data = [] 
     for point in selected_data["points"]:
         id = point["customdata"]
