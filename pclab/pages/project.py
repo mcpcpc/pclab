7b53fadc57439a089482ac02f723aa131c8c7914
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-07-02 17:45:40 -0400

    update

--- pclab/pages/project.py
+++ pclab/pages/project.py
@@ -132,11 +132,14 @@ def update_row_request(request, selected_data):
     if request is None:
         return no_update
     if selected_data is None:
+        print("not initialized")
         #return no_update
         return {"rowData": [], "rowCount": 0}
     if not isinstance(selected_data["points"], list):
+        print("points do not exist")
         return {"rowData": [], "rowCount": 0}
     if len(selected_data["points"]) < 1:
+        print("not points selected")
         #return no_update
         return {"rowData": [], "rowCount": 0}
     data = [] 
